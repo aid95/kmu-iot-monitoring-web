6050ba1ddc823e7868d702df904a5f2c49e87eff
imgomi <webmaster@mail.gomi.land>
2020-06-06 22:21:38 +0900

    로그인 페이지 파일 이동

--- nginx/html/member/signin.php
+++ nginx/html/member/signin.php
@@ -5,7 +5,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=1280, initial-scale=1.0">
-    <title>FLORA 🌷 로그인</title>
+    <title>FLORA - 로그인</title>
 
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
