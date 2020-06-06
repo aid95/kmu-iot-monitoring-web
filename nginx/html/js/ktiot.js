e0a710df19fe3b6a67693ab8bc8c720975360fe8
imgomi <webmaster@mail.gomi.land>
2020-06-06 11:12:06 +0900

    KT IoTMakers JS 모듈 주석 작성

--- nginx/html/js/ktiot.js
+++ nginx/html/js/ktiot.js
@@ -1,5 +1,6 @@
 let ktiot = (function (ktiot, $) {
-    const DEVICE_ID = "imgomiD1591093976479";
+    // START: KT IoTMakers와 통신하기 위해서 필요한 전역 변수
+    const DEVICE_ID                 = "imgomiD1591093976479";
 
     const REST_API_HOST_URL         = "https://iotmakers.kt.com";
 
@@ -9,10 +10,22 @@ let ktiot = (function (ktiot, $) {
     const TAG_STREAM_LOG_BASE_TIME  = `api/v1/streams/${DEVICE_ID}/log`;
     const TAG_STREAM_LOG_DETAIL     = `api/v1/streams/${DEVICE_ID}`;
     const TAG_STREAM_LOG_LAST       = `api/v1/streams/${DEVICE_ID}/log/last`;
+    // END: KT IoTMakers와 통신하기 위해서 필요한 전역 변수
 
+    // START: 초기화 상태와 유저 토큰
     let IS_INIT = false;
     let USER_TOKEN;
+    // END: 초기화 상태와 유저 토큰
 
+    /**
+     *  @description 태그 스트림 데이터 가져오기
+     * 
+     *  @author: 신병주(webmaster@mail.gomi.land)
+     *  @param:
+     *      - count: 태그 스트림 데이터 개수
+     *      - period: 태그 스트림의 데이터 분
+     *  @return: 태그 스트림 딕셔너리 데이터 배열
+     */
     ktiot.get_tag_stream_period = (count, period) => {
         if (!IS_INIT) {
             ktiot.init();
@@ -27,6 +40,16 @@ let ktiot = (function (ktiot, $) {
         );
     };
 
+    /**
+     *  @description from~to까지의 태그 스트림 데이터 가져오기
+     * 
+     *  @author: 신병주(webmaster@mail.gomi.land)
+     *  @param:
+     *      - count: 태그 스트림 데이터의 개수
+     *      - from: 시작 시간
+     *      - to: 끝 시간
+     *  @return: 태그 스트림 딕셔너리 데이터 배열
+     */
     ktiot.get_tag_stream_until = (count, from, to) => {
         if (!IS_INIT) {
             ktiot.init();
@@ -42,6 +65,13 @@ let ktiot = (function (ktiot, $) {
         );
     };
 
+    /**
+     *  @description 마지막 요청된 태그 스트림 가져오기
+     * 
+     *  @author: 신병주(webmaster@mail.gomi.land)
+     *  @param: void
+     *  @return: 태그스트림 데이터
+     */
     ktiot.get_last_tag_stream = () => {
         if (!IS_INIT) {
             ktiot.init();
@@ -53,6 +83,13 @@ let ktiot = (function (ktiot, $) {
         );
     };
 
+    /**
+     *  @description 태그 스트림 값 자세히 가져오기
+     * 
+     *  @author: 신병주(webmaster@mail.gomi.land)
+     *  @param: void
+     *  @return: 태그스트림 데이터
+     */
     ktiot.get_tag_stream = () => {
         if (!IS_INIT) {
             ktiot.init();
@@ -63,11 +100,45 @@ let ktiot = (function (ktiot, $) {
         )
     };
 
+    /**
+     *  @description KT IoTMakers를 사용하기 위한 초기화 함수
+     * 
+     *  @author: 신병주(webmaster@mail.gomi.land)
+     *  @param: void
+     *  @return: void
+     */
     ktiot.init = () => {
         request_token_aware_api();
         IS_INIT = true;
     };
 
+    /**
+     *  @description 딕셔너리 배열을 키값별 데이터 배열로 변환하는 함수
+     * 
+     *  @author: 신병주(webmaster@mail.gomi.land)
+     *  @param: 딕셔너리 배열
+     *  @return: 키값으로 구별된 데이터 딕셔너리 배열
+     */
+    ktiot.split_dict_array = (dict_array) => {
+        let result = {};
+        for (let i = 0; i < dict_array.length; i++) {
+            for (const [key, value] of Object.entries(dict_array[i].attributes)) {
+                if (!result.hasOwnProperty(key)) {
+                    result[key] = [];
+                }
+                result[key].push(value); 
+            }
+        }
+        return result;
+    }
+
+    /**
+     *  @description RestAPI 사용을 위한 토큰 정보를 가져오기 위한 함수
+     * 
+     *  @author: 신병주(webmaster@mail.gomi.land)
+     *  @param: void
+     *  @return: void
+     */
     function request_token_aware_api() {
         send(
             'POST',
@@ -86,6 +157,19 @@ let ktiot = (function (ktiot, $) {
         );
     };
 
+    /**
+     *  @description Ajax를 간편하기 사용하기 위한 함수
+     * 
+     *  @author: 신병주(webmaster@mail.gomi.land)
+     *  @param:
+     *      - method: 메소드명
+     *      - url: 요청할 URL 주소
+     *      - headers: 요청시 포함할 헤더정보
+     *      - data: 요청시 포함할 데이터
+     *      - fn: 통신이 성공할 경우 호출될 함수
+     *      - async: 비동기화 통신 Flag
+     *  @return: void
+     */
     function send(method, url, headers, data, fn, async=false) {
         $.ajax(url, {
             async,
@@ -97,6 +181,15 @@ let ktiot = (function (ktiot, $) {
         });
     }
 
+    /**
+     *  @description GET method만을 위한 요청 래핑 함수
+     * 
+     *  @author: 신병주(webmaster@mail.gomi.land)
+     *  @param:
+     *      - url: 요청할 URL 주소
+     *      - params: 요청시 포함할 데이터
+     *  @return: 요청에 대한 response 데이터
+     */
     function api_method_get_send(url, params) {
         let ret;
         send(
@@ -111,6 +204,14 @@ let ktiot = (function (ktiot, $) {
         return ret;
     }
 
+    /**
+     *  @description 시간 문자열을 timestamp값으로 반환
+     * 
+     *  @author: 신병주(webmaster@mail.gomi.land)
+     *  @param:
+     *      - tstr: 변환할 시간 문자열
+     *  @return: unix-timestamp
+     */
     function conv_time_to_timestamp(tstr) {
         return new Date(tstr).getTime();
     }
