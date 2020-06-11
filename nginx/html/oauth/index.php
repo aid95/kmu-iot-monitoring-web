e816a90c11f8d548f8b894a66578eb4f80219a83
imgomi <webmaster@mail.gomi.land>
2020-06-12 03:05:49 +0900

    카카오 REST API 사용을 위한 코드 추가

---
+++ nginx/html/oauth/index.php
@@ -0,0 +1,16 @@
+<?php
+
+    $authorize_code = $_GET["code"];
+    $link = mysqli_connect("db", "root", "sksmschlrhek1", "iotadmin_db");
+    if (!$link) {
+        exit;
+    }
+    $sql = "SELECT * FROM flora WHERE name='imgomi'";
+    $result = mysqli_query($link, $sql);
+    $num_record = mysqli_num_rows($result);
+    if ($num_record >= 1) {
+        $sql = "UPDATE flora SET code='$authorize_code', WHERE name='imgomi'";
+    } else {
+        $sql = "INSERT INTO flora(name, code) VALUES('imgomi', '$authorize_code')";
+    }
+    echo "<script> window.location.href='/'</script>";
---
+++ plugin/kakao_api.py
@@ -0,0 +1,56 @@
+import requests
+import pymysql
+import json
+
+class KakaoAPI:
+
+    def __init__(self):
+        self.CLIENT_ID = 'e88dc6e8a3c14d8a611dbb9d511d4cf9'
+        self.REDIRECT_URI = 'http://flora.gomi.land/oauth'
+        self.ACCESS_TOKEN = ''
+        self.REFRESH_TOKEN = ''
+        self.DB = pymysql.connect(host='db', port=3306, user='root', passwd='sksmschlrhek1', db='iotadmin_db', charset='utf8')
+        self.CURS = self.DB.cursor(pymysql.cursors.DictCursor)
+
+    def set_tokens_from_db():
+        sql = "SELECT * FROM flora WHERE name=%s"
+        curs.execute(sql, ('imgomi'))
+        rows = curs.fetchall()
+
+        url = 'https://kauth.kakao.com/oauth/token'
+        code = rows[0]['code']
+        d = {
+            'grant_type': 'authorization_code',
+            'client_id': self.CLIENT_ID,
+            'redirect_uri': self.REDIRECT_URI,
+            'code': code
+        }
+        x = requests.post(url, data=d)
+        json_data = json.loads(x.text)
+        self.ACCESS_TOKEN = json_data['access_token']
+        self.REFRESH_TOKEN = json_data['refresh_token']
+
+    def get_access_token():
+        return self.ACCESS_TOKEN
+
+    def get_refresh_token():
+        return self.REFRESH_TOKEN
+
+    def send_template_message_for_me(template_id)
+        if self.ACCESS_TOKEN == '':
+            return
+        url = 'https://kapi.kakao.com/v2/api/talk/memo/send'
+        h = {'Authorization': 'Bearer {}'.format(self.ACCESS_TOKEN)}
+        d = {'template_id': '30087'}
+        x = requests.post(url, headers=h, data=d)
+
+
+def main():
+    template_list = {'water': '30086', 'battery': '30087'}
+    kakao = KakaoAPI()
+    kakao.set_tokens_from_db()
+    print(kakao.get_access_token())
+
+
+if __name__ == '__main__':
+    main()
